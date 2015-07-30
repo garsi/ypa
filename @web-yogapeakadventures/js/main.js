7f128a06da8ab80b74710ec8f3c25ba9ee3430ba
Kat Garsi <katgarsi@gmail.com>
2015-07-29 21:29:26 -0700

    Finalize responsive background image, add js fallbacks and make adjustments from testing

--- @web-yogapeakadventures/js/main.js
+++ @web-yogapeakadventures/js/main.js
@@ -1,1 +1,30 @@
+// Released under MIT license: http://www.opensource.org/licenses/mit-license.php
 
+// good javascript
+(function ($) {
+    'use strict';
+    $(function () {
+        $('[placeholder]')
+          .focus(function () {
+              var input = $(this);
+              if (input.val() === input.attr('placeholder')) {
+                  input.val('').removeClass('placeholder');
+              }
+          })
+          .blur(function () {
+              var input = $(this);
+              if (input.val() === '' || input.val() === input.attr('placeholder')) {
+                  input.addClass('placeholder').val(input.attr('placeholder'));
+              }
+          })
+          .blur()
+          .parents('form').submit(function () {
+              $(this).find('[placeholder]').each(function () {
+                  var input = $(this);
+                  if (input.val() === input.attr('placeholder')) {
+                      input.val('');
+                  }
+              });
+          });
+    });
+})(jQuery);
